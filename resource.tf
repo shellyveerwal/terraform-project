1c34637d17eb2ff39287327d31a36480882b40f5
Vishantveer <vishantveerwal@gmail.com>
2024-01-10 23:05:26 +0530

    first commit

---
+++ resource.tf
@@ -0,0 +1,10 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+
+resource "aws_instance" "demo-server" {
+  ami           = "ami-0005e0cfe09cc9050"
+  instance_type = "t2.micro"
+  key_name      = "dpp"
+}
